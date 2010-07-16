03bd9ab868602a8384baaee0297521528c4569f5
cgeek <cem.moreau@gmail.com>
2010-07-16 22:22:38 +0100

    Réorganisation du dossier

--- booster/booster.php
+++ booster/booster.php
@@ -3,12 +3,12 @@
 header('Content-type: text/xml');
 $sourceXML = my_file_get_contents("nead.univ-angers.fr", "/celcat/istia/g2096.xml");
 define("EN_MODE_TEST", false);
+define("ANGLAIS_GROUPE", "EI3/TC/G1");
 
 if(EN_MODE_TEST){
 	
 	// TESTS
 	$filtres = array("EI3", "EI3/AGI", "EI3/II", "EI3/AGI/G1", "EI3/II/G1", "EI3/TC/G1", "EI3/AGI/TPAGI1", "EI3/II/TPII2", "EI3/s6/Espagnol/TD G2");
-	define("ANGLAIS_GROUPE", "EI3/TC/G1");
 	echo CelcatAfficheContenuFiltre($sourceXML, $filtres, ANGLAIS_GROUPE);
 }
 else{
