9bdb5facbd4a66053975cf3c1ad7899c1a891508
Cédric Moreau <cem.moreau@gmail.com>
2011-09-04 14:03:48 +0200

    Avant modifs de pré-rentrée

--- js/Arborescence.js
+++ js/Arborescence.js
@@ -7,86 +7,83 @@ window.addEvent('domready', function(){
 	
 	racine.setStyle('display', 'inline');
 	
-	var nodeIt = function(nodeJSON) {
-		var node = new Noeud(nodeJSON.libelle, nodeJSON.identifiant, new Hash(nodeJSON.extra));
-		nodeJSON.categories.each(function(categorieJSON) {
-			var categorie = new Categorie(categorieJSON.libelle, categorieJSON.rubrique);
-			categorieJSON.noeuds.each(function(noeud) {
-				categorie.add(nodeIt(noeud));
-			});
-			node.addCategorie(categorie);
-		});
-		return node;
-	}
-	
 	new Request({
 		url: "include/promotions.php",
 		onComplete: function(resultatJSON){
-			var noeud = JSON.decode(resultatJSON);
-			nodeIt(noeud).affiche(racine);
+			var resultat = JSON.decode(resultatJSON);
+			data2nodes(resultat).affiche(racine);
 			if(chargeur.chargeDepuisCookie())
 				$('lienInterne').fireEvent('click');
 		}
 	}).get();
 	
-	var genereHashDeSelection = function() {
-		var hash = new Hash();
-		var selects = $$('select'); 
-		var nbSelectsNonNuls = 0;
-		selects.each(function(select) {
-			if(select.selectedIndex != 0)
-				nbSelectsNonNuls++;
-		})
-		selects.each(function(select) {
-			if(select.selectedIndex > 0){
-				var extraValue = select.options[select.selectedIndex].get('edt');
-				if($defined(extraValue))
-					hash.set('edt', extraValue);
-				if(nbSelectsNonNuls > 0){
-					var oldVal = hash.get(select.get('rubrique'));
-					var newVal = (oldVal == null) ? select.get('value') : oldVal + ";" + select.get('value');
-					hash.set(select.get('rubrique'), newVal);
-				}
-			}
-		});
-		return hash;
-	}
-
-	var genereURI = function() {
-		var base = genereHashDeSelection().toQueryString();
-		return base.replace("%C3%A9", "%E9");
-	}
-
-	var genereMessageHash = function() {
-		var hash = genereHashDeSelection();
-		var message = "";
-		hash.each(function(valeur, cle) {
-			message += cle + " = " + valeur + "\n";
-		});
-		return message;
-	}
-	
-	var getAction = function() {
-		return formulaire.get('action');
-	}
-	
 	$('lienInterne').addEvent('click', function(){
 		var URI = genereURI();
 		chargeur.sauveVersCookie(URI);
 		var maFrame = $('celcatboosterInnerFrame');
-		maFrame.set('src', getAction() + "?" + URI);
+		maFrame.set('src', formulaire.get('action') + "?" + URI);
 		return false;
 	});
 	
 	$('lienExterne').addEvent('click', function(){
 		var URI = genereURI();
 		chargeur.sauveVersCookie(URI);
-		this.set('href', getAction() + "?" + URI);
+		this.set('href', formulaire.get('action') + "?" + URI);
 	});
 	
 	$('lienICal').addEvent('click', function(){
 		var URI = genereURI();
+		this.set('href', formulaire.get('action') + "?" + URI + "&format=ical");
 		chargeur.sauveVersCookie(URI);
-		this.set('href', getAction() + "?" + URI + "&format=ical");
 	});
 });
+
+function data2nodes(nodeJSON) {
+	var node = new Noeud(nodeJSON.libelle, nodeJSON.identifiant, new Hash(nodeJSON.extra));
+	nodeJSON.categories.each(function(categorieJSON) {
+		var categorie = new Categorie(categorieJSON.libelle, categorieJSON.rubrique);
+		categorieJSON.noeuds.each(function(noeud) {
+			categorie.add(data2nodes(noeud));
+		});
+		node.addCategorie(categorie);
+	});
+	return node;
+}
+
+function genereHashDeSelection() {
+	var hash = new Hash();
+	var selects = $$('select'); 
+	var nbSelectsNonNuls = 0;
+	selects.each(function(select) {
+		if(select.selectedIndex != 0)
+			nbSelectsNonNuls++;
+	})
+	selects.each(function(select) {
+		if(select.selectedIndex > 0){
+			var extraValue = select.options[select.selectedIndex].get('edt');
+			if($defined(extraValue))
+				hash.set('edt', extraValue);
+			if(nbSelectsNonNuls > 0){
+				var oldVal = hash.get(select.get('rubrique'));
+				var newVal = (oldVal == null) ? select.get('value') : oldVal + ";" + select.get('value');
+				hash.set(select.get('rubrique'), newVal);
+			}
+		}
+	});
+	return hash;
+}
+
+function genereURI() {
+	var base = genereHashDeSelection().toQueryString();
+	return base.replace("%C3%A9", "%E9");
+}
+
+function genereMessageHash() {
+	var hash = genereHashDeSelection();
+	var message = "";
+	hash.each(function(valeur, cle) {
+		message += cle + " = " + valeur + "\n";
+	});
+	return message;
+}
+
