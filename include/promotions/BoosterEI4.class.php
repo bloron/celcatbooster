3833c718385784c1163b5c7151c12dce382721b9
Mathieu Beuremila <beuremila.mathieu@gmail.com>
2012-09-20 12:33:05 +0200

    Mise en place des groupes de communication et de droit pour les Ei4 Correction sur les groupes d'anglais pour les Ei5

--- include/promotions/BoosterEI4.class.php
+++ include/promotions/BoosterEI4.class.php
@@ -2,14 +2,16 @@
 class BoosterEI4 extends Booster {
 	
 	public function __construct(){
-		parent::__construct("g15807");
+		parent::__construct("g15807",array(
+            "communication" => "filterComm",
+            "droit du travail" => "filterComm"));
 	}
 
-	/*protected function filterComm(SimpleXMLElement $cours){
+	protected function filterComm(SimpleXMLElement $cours){
 		if($this->groupesCorrespondent($this->groupes['gpCom'], $cours->resources->group))
 			return true;
 		return false;
-	}*/
+	}
         
     
 }
--- include/promotions/BoosterEI5.class.php
+++ include/promotions/BoosterEI5.class.php
@@ -20,9 +20,9 @@ class BoosterEI5 extends Booster {
         if ($this->issetGroup(self::$GROUPE_ANGLAIS)) {
             foreach ($this->groupes[self::$GROUPE_ANGLAIS] as $monGroupe) {
                 $monGroupe = strtolower(trim($monGroupe));
-                $monProf = (in_array("FERRIER,Catherine", $profs) && strpos($this->groupesStrings[self::$GROUPES_GENERAUX], "AGI"));
-                $monProf = $monProf ||
-                        (in_array("REVERDY,Valerie", $profs) && strpos($this->groupesStrings[self::$GROUPES_GENERAUX], "QSF"));
+                $monProf = (strpos($this->groupesStrings[self::$GROUPES_GENERAUX], "AGI"));
+                $monProf = $monProf || (strpos($this->groupesStrings[self::$GROUPES_GENERAUX], "QSF"));
+                $monProf = $monProf || (strpos($this->groupesStrings[self::$GROUPES_GENERAUX], "INNO"));
                 $matched = ($matched OR ($monProf AND strpos($remarque, $monGroupe) !== false));
             }
         }
@@ -30,7 +30,7 @@ class BoosterEI5 extends Booster {
             // On les affiche tous
             $matched = true;
         }
-        return $matched OR $this->groupesCorrespondent("EI5 AGI", $cours->resources->group);
+        return $matched;
     }
 
     /* Les EI5 actuels (2011-2012) ont Allemand et Espagnol en même temps */
@@ -42,13 +42,4 @@ class BoosterEI5 extends Booster {
     protected function concerneMonGroupeEspagnol(SimpleXMLElement $cours) {
         return !$this->pasDeSecondeLangue();
     }
-
-    /*     * *** REDEFINITION pour la gestion de groupes ultra spécifiques (EI5 AGI pour définir EI5 IAIE et EI5 IHM-RV) **** */
-
-    protected function groupesCorrespondent($mesGroupes, SimpleXMLElement $lesGroupesDuCours) {
-        $groupesAppartenance = (is_array($mesGroupes)) ? $mesGroupes : array($mesGroupes);
-        $groupesAppartenance[] = "EI5 AGI";
-        return parent::groupesCorrespondent($groupesAppartenance, $lesGroupesDuCours);
-    }
-
 }
