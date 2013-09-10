ffc3a86160f4ad05ee0a968d6765c031b185232b
bastien.loron <>
2013-09-11 01:29:24 +0200

    Mise à jour des groupesMise à jour des URLs des fichiers ressources

--- include/Fabrique.class.php
+++ include/Fabrique.class.php
@@ -60,7 +60,7 @@ class Fabrique {
 		$formatter = null;
 		switch($type){
             case self::$ICAL :
-                $icsData = self::getFile("/istia/" . $booster->getResource() . ".ics");
+                $icsData = self::getFile("/bb/" . $booster->getResource() . ".ics");
                 $formatter = new ICal($icsData); break;
             case self::$XML :
 			default : 
